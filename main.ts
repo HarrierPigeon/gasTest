69dc28376052c22d661816aae15cc795fbcb768c
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-10-21 14:17:58 -0500

    cleaning up stuff

--- main.ts
+++ main.ts
@@ -15,15 +15,21 @@ let sheetConfigInfo: sheetDataEntry = {
     tabName: 'search',
     headerRow: 5,
     initialColumnOrder: {
-        id: 0,
-        url: 1,
-        title: 2,
-        channel: 3,
-        pubDate:4
+        pubDate:0,
+        title: 1,
+        channel: 2,
+        url: 3,
+        id: 4,
     },
     includeSoftcodedColumns: false
 }
 
+// thanks to https://stackoverflow.com/questions/784586/convert-special-characters-to-html-in-javascript
+function decodeHtmlCharCodes(str):string {
+    return str.replace(/(&#(\d+);)/g, function (match, capture, charCode) {
+        return String.fromCharCode(charCode);
+    });
+}
 
 function runSearch() {
     let searchSheet = new SheetData(new RawSheetData(sheetConfigInfo))
@@ -63,12 +69,16 @@ function search(query: string,numResults:number): searchResult[] | undefined {
             channel: '',
             pubDate: ''
         }
+        
         output.id = String(item.id.videoId)
         output.url = "https://youtube.com/watch?v=" + String(item.id.videoId)
-        output.title = String(item.snippet.title)
-        output.channel = String(item.snippet?.channelTitle)
+        output.title = decodeHtmlCharCodes(String(item.snippet.title))
+        output.channel = decodeHtmlCharCodes(String(item.snippet.channelTitle))
         output.pubDate = new Date(item.snippet.publishedAt)
-        results.push(output)
+        if (output.id != "undefined") {
+            results.push(output)
+            
+        }
         console.log(item["kind"])
         console.log(item["etag"])
         
