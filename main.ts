7936ab5333074228511fc4cafe4b9f9c0ec09036
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-10-21 13:44:19 -0500

    kicked out sheetCore, trying to install types and its not working :(

--- main.ts
+++ main.ts
@@ -3,6 +3,22 @@ function checkUpdate() {
 }
 
 
-function search(query: string) {
+interface searchResult {
+    id: string,
+    url: string,
+    title: string,
+    channel: string,
     
+}
+
+function search(query: string) {
+    let result:searchResult = {
+        id:""
+    }
+
+    let results: searchResult[] = []
+    
+    try {
+        const unparsed = 
+    }
 }
