51f795af49d9eebbaedb7c1bbbcfe8012ede9d6b
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-02-17 12:51:19 -0600

    updating, starting work on curlybird

---
+++ curlybird.ts
@@ -0,0 +1,18 @@
+function test1(){
+    let config: sheetDataEntry = {
+        tabName: 'curlyBird',
+        headerRow: 5,
+        initialColumnOrder: {
+            line1: 0,
+            line2:1
+        },
+        includeSoftcodedColumns: true
+    }
+    let url = "https://github.com/texas-mcallen-mission/sheetCore"
+    let fetchArgs: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        muteHttpExceptions: true,
+    }
+    let test2 = UrlFetchApp.fetch(url, fetchArgs)
+    let response = JSON.parse(test2.getContentText())
+    console.log(test2);
+}
