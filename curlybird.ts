73547101a53d5c059e90d2097792c8f719fdac0a
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-02-17 13:07:48 -0600

    okay, please work, Mr. Action Man

--- curlybird.ts
+++ curlybird.ts
@@ -8,12 +8,27 @@ function test1(){
         },
         includeSoftcodedColumns: true
     }
-    let url = "https://github.com/texas-mcallen-mission/sheetCore"
+
+    let url1 = "https://github.com/texas-mcallen-mission/sheetCore"
     let fetchArgs: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
         muteHttpExceptions: true,
     }
-    let test2 = UrlFetchApp.fetch(url, fetchArgs)
-    let response = JSON.parse(test2.getContentText())
-    console.log(response);
+    console.log(getDataFromURL(url1))
+    let url2 = composeGitUrl("texas-mcallen-mission", "sheetCore")
+    console.log(getDataFromURL(url2))
+}
+
+function getDataFromURL(url: string) {
+    let fetchArgs: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        muteHttpExceptions: true,
+    }
+    const fetcher = UrlFetchApp(url, fetchArgs)
+    let response = JSON.parse(fetcher.getContentText())
+    return response
+}
+
+function composeGitUrl(ownerName:string,repoName:string) {
+    const baseURL = "https://github.com"
     
+    return baseURL + "/"+ownerName + "/"+repoName
 }
